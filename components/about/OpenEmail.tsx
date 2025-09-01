45385f6e42b80331db8f569ce96bfc87e12b752e
JustTrap <nebamulle7@gmail.com>
2025-09-01 19:05:01 +0300

    Fix Gmail compose URL to properly pre-fill recipient email address using view=cm format

--- components/about/OpenEmail.tsx
+++ components/about/OpenEmail.tsx
@@ -6,7 +6,7 @@ import React from "react";
 export default function OpenEmail() {
   return (
     <button
-      onClick={() => window.open("https://mail.google.com/mail/u/0/#compose?to=chanolymarketing@gmail.com", "_blank")}
+      onClick={() => window.open("https://mail.google.com/mail/?view=cm&fs=1&to=chanolymarketing@gmail.com", "_blank")}
       className="flex items-center hover:text-white transition-all duration-300 cursor-pointer font-medium group"
     >
       <Mail className="w-5 h-5 mr-3 group-hover:text-chanoly transition-colors duration-300" />
--- components/branches/BranchesFooter.tsx
+++ components/branches/BranchesFooter.tsx
@@ -110,7 +110,7 @@ export function BranchesFooter() {
             </h4>
             <div className="space-y-2 text-gray-400 text-sm">
                              <button
-                 onClick={() => window.open("https://mail.google.com/mail/u/0/#compose?to=chanolymarketing@gmail.com", "_blank")}
+                 onClick={() => window.open("https://mail.google.com/mail/?view=cm&fs=1&to=chanolymarketing@gmail.com", "_blank")}
                  className="flex items-center hover:text-white transition-all duration-300 cursor-pointer font-medium group"
                >
                 <Mail className="w-5 h-5 mr-3 group-hover:text-chanoly transition-colors duration-300" />
